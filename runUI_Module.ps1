20fba06d673eb3babd743518ea2d19c61b3e2ea4
sepppirch <sepppirch@gmail.com>
2021-01-18 18:23:15 +0100

    Update requirements.txt and runUI_Module.ps1

--- runUI_Module.ps1
+++ runUI_Module.ps1
@@ -1,7 +1,12 @@
 $host.ui.RawUI.WindowTitle = 'UI_Server'
 $host.UI.RawUI.BackgroundColor='darkblue'
 Clear-Host
+#create virtual env in windows and activate it and install requirements.txt
+py -3 -m venv venv
 venv\Scripts\activate
+#pip install flask_cors
+#pip install pymysql
+python -m pip install -r requirements.txt
 $env:FLASK_ENV="development"
 $env:FLASK_APP="json_io.py"
 flask run
