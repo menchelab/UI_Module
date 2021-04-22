f78662e2133d3526d4e8d4b46294bbf0efaf24d6
sebastian <sebastian@OMEN.local>
2021-04-22 16:20:13 +0200

    added map

--- static/js/nodepanel_UI.js
+++ static/js/nodepanel_UI.js
@@ -10,5 +10,26 @@ $(document).ready(function () {
     });
 
 
-
+    logger("im there");
+    redrawMap(48.14, 16.46);
+    
+   
 });
+
+function redrawMap(lat, lon){
+
+
+   // $("#map").css("width",$(window).width()).css("height",$(window).height());
+    var map = L.map('map', { zoomControl: false }).setView([lat, lon], 13);
+    
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> contributors',
+        maxZoom: 18,
+    }).addTo(map);
+    
+   // map.on('click', onMapClick);
+  
+}
+
+
+
