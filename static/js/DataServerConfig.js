d5ed23f49e7526714e308b7dba5169ed9ceba640
sebastian <sebastian@OMEN.local>
2021-04-26 14:55:44 +0200

    Update DataServerConfig.js

--- static/js/DataServerConfig.js
+++
@@ -1,4 +0,0 @@
-//var dbprefix = 'http://asimov.westeurope.cloudapp.azure.com:8887';
-var dbprefix = 'http://127.0.0.1:1337';
-var dbuser = 'steveballmer';
-var dbpw = 'code peaceful canon shorter';
